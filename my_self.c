d33b0c2fe18bd2498a803b0ec2d898e713ccdcf8
Rehan Shaikh <r.123shaikhh@gmail.com>
2025-10-15 20:37:36 +0530

    Add files via upload

---
+++ my_self.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    /**
+     * NAME = REHAN AKHTAR ALI SHAIKH
+     * UIN 251P038
+     * ROLL NO 38
+     * 
+     */
+
+
+    printf("Name = Rehan Shaikh \n");
+    printf("Age = 18 \n");
+    printf("Division = C \n");
+    printf("Class = Computer C01 \n");
+    printf("UIN = 251P038 \n");
+
+    return 0;
+}
