b33cc128c396fcf8b94de5d5d7ffe9e2a47645f0
Rehan Shaikh <r.123shaikhh@gmail.com>
2025-10-15 20:26:08 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+/*
+NAME=REHAN AKHTAR ALI SHAIKH
+DIVISION=C
+UIN=251P038
+*/
+{
+    int num1;
+    printf("Enter the number :");
+    scanf("%d",&num1);
+    if (num1 %2==0 ){
+        printf("it is a even number ");
+    }
+    
+    else {
+        printf("it is odd number ");
+    }
+}
+
